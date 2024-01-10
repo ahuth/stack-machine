2d76fac615103a55608b5cb09f7b51191e8b8939
Andrew Huth <git@huth.me>
2024-01-10 07:29:32 -0500

    Remove "run" button for now

--- src/components/Controls.tsx
+++ src/components/Controls.tsx
@@ -23,11 +23,6 @@ export default function Controls({dispatch}: Props) {
           ▶️ Step
         </button>
       </li>
-      <li>
-        <button className="border border-slate-950 p-1 hover:bg-slate-100 active:translate-y-1 active:bg-slate-200">
-          ⏩ Run
-        </button>
-      </li>
     </ul>
   );
 }
